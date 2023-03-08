4de131fec16bf2220b32cb96a924b1fc14e7c6b7
alexo <alexola.22@gmail.com>
2023-03-08 15:34:27 +0000

    99 project finish and explained

---
+++ 99Bottles.py
@@ -0,0 +1,9 @@
+# You can go crazy and put 99 but it will take time to get to 0 xD 
+# AS you can see the range basically says 10 -> 0 with a -1 every time you can modify it anyway you want
+for i in range(10, 0 ,-1):
+  print(str(i) + ' bottles of beer on the wall ')
+  print('')
+  print(str(i) + ' bottles of beer '  )
+  print('')
+  print('Take one down, pass it around')
+  print('')
